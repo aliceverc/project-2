1f5ae9aca118dd72f65ced289bcb7d7b3159f4fd
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-09 12:08:15 +0200

    add toggle functionality to the answer buttons

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// // ADD TOGGLE FUNCTIONALITY to bookmark buttons
+
 // querySelectorAll > returns a list of all matching elements
 const bookmarkButtons = document.querySelectorAll(
   '[data-js="button-bookmark"]'
@@ -5,8 +7,24 @@ const bookmarkButtons = document.querySelectorAll(
 
 // forEach > goes through each element in that list one by one
 bookmarkButtons.forEach((button) => {
-    //add a click event listener to each button that toggles the "bookmark--active" class
-    button.addEventListener("click", () => {
-        button.classList.toggle("bookmark--active");
-    });
+  //add a click event listener to each button that toggles the "bookmark--active" class
+  button.addEventListener("click", () => {
+    button.classList.toggle("bookmark--active");
+  });
+});
+
+// ADD TOGGLE FUNCTIONALITY to bookmark buttons
+
+const cardButtonAnswer = document.querySelectorAll(
+  '[data-js="card__button-answer"]'
+);
+const card = document.querySelectorAll('[data-js="card__answer"]');
+
+cardButtonAnswer.forEach((button) => {
+  button.addEventListener("click", () => {
+    button
+      .closest(".card")
+      .querySelector('[data-js="card__answer"]')
+      .classList.toggle("card__answer--active");
+  });
 });
