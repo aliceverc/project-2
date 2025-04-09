f93b002324688f2a8fc11aa484b464fb813f133d
Alice Vercelli <alicevercelli13@gmail.com>
2025-04-09 11:27:39 +0200

    add toggle functionality to the bookmark buttons

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// phew… not a lot going on here. Please add some code!
+// querySelectorAll > returns a list of all matching elements
+const bookmarkButtons = document.querySelectorAll(
+  '[data-js="button-bookmark"]'
+);
+
+// forEach > goes through each element in that list one by one
+bookmarkButtons.forEach((button) => {
+    //add a click event listener to each button that toggles the "bookmark--active" class
+    button.addEventListener("click", () => {
+        button.classList.toggle("bookmark--active");
+    });
+});
